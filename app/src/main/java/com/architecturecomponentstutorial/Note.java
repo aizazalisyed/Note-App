58d54bcbafd8b7836ef8377d8349ba4e279fb19c
aizazalisyed <aizazalisyed.outlook.com>
2022-12-20 13:30:23 +0500

    app component architecture.

---
+++ app/src/main/java/com/architecturecomponentstutorial/Note.java
@@ -0,0 +1,44 @@
+package com.architecturecomponentstutorial;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "note_table")
+public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    private String description;
+
+    private int priority;
+
+
+}
---
+++ app/src/main/java/com/architecturecomponentstutorial/NoteDao.java
@@ -0,0 +1,6 @@
+package com.architecturecomponentstutorial;
+
+public interface NoteDao {
+
+
+}
