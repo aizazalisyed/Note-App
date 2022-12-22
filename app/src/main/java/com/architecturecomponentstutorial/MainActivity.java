6980d09fd8c36d85a5fc3ec2ea14f48ccfc3b25a
aizazalisyed <aizazalisyed.outlook.com>
2022-12-22 10:21:13 +0500

    added view model

--- app/src/main/java/com/architecturecomponentstutorial/MainActivity.java
+++ app/src/main/java/com/architecturecomponentstutorial/MainActivity.java
@@ -1,14 +1,22 @@
 package com.architecturecomponentstutorial;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, notes -> {
+            //update recyclerview
+        });
     }
 }
