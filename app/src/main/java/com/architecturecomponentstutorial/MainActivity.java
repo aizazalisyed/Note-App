a787cd9d5e3b1d532fa4065701218d067624797d
aizazalisyed <aizazalisyed.outlook.com>
2022-12-25 15:41:09 +0500

    added recycler view

--- app/src/main/java/com/architecturecomponentstutorial/MainActivity.java
+++ app/src/main/java/com/architecturecomponentstutorial/MainActivity.java
@@ -1,10 +1,13 @@
 package com.architecturecomponentstutorial;
 
+import androidx.annotation.NonNull;
 import  androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
-        noteViewModel.getAllNotes().observe(this, notes -> {
-            //update recyclerview
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+
+            }
         });
     }
 }
